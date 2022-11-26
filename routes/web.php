dd79650e674e9dcdd5a356eeac8a61bcc07bcfa7
Asif Ahamed <asifshezan7@gmail.com>
2022-11-26 22:08:03 +0600

    Route, Model, Controller create

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,29 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [TaskController::class, 'index'])->name('task.index');
+Route::post('/task/store', [TaskController::class, 'store'])->name('task.store');
+Route::put('/task/update/{id}', [TaskController::class, 'update'])->name('task.update');
+Route::post('/task/delete/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
