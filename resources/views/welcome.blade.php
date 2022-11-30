5c2deec6031db080001cd9442f1367f8436f3562
Asif Ahamed <asifshezan7@gmail.com>
2022-11-30 21:07:19 +0600

    Edit Modal add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                                         <td> {{ $task->id }} </td>
                                         <td> {{ $task->name }} </td>
                                         <td style="width: 250px;">
-                                            <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                            <a href="#"  data-toggle="modal" data-target="#editTask" class="btn btn-sm btn-primary">Edit</a>
                                             <a href="#" class="btn btn-sm btn-danger">Delete</a>
                                         </td>
                                     </tr>
@@ -60,7 +60,7 @@
     </section>
 
 
-<!-- Modal -->
+<!-- Create Modal -->
 <div class="modal fade" id="createTask"  role="dialog" tabindex="-1" aria-labelledby="createTaskTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
@@ -86,7 +86,35 @@
     </div>
     </div>
 </div>
+<!--End Create Modal -->
 
+<!-- Edit Modal -->
+<div class="modal fade" id="editTask"  role="dialog" tabindex="-1" aria-labelledby="editTaskTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+        <form action="" id="editTaskForm">
+        <div class="modal-header">
+        <h5 class="modal-title" id="editTaskTitle">Edit Task</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="modal-body">
+            <div id="editTaskMessage"></div>
+            <div class="form-group">
+                <label for="">Enter Task Name</label><br>
+                <input type="text" class="form-control" name="name" placeholder="Enter Task Name">
+            </div>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-success">Update Task</button>
+        </div>
+    </form>
+    </div>
+    </div>
+</div>
+<!--End Create Modal -->
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
